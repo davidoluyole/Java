6db7aa12c1f8893c5351b3575fabc72c770be963
davidoluyole <103898834+davidoluyole@users.noreply.github.com>
2022-05-15 18:40:36 +0100

    Add files via upload

---
+++ Answer2_14.java
@@ -0,0 +1,13 @@
+public class Answer2_14{
+
+	public static void main(String... args){
+	
+	System.out.println("1 2 3 4");
+
+	System.out.print("1 2 3 4 \n");
+
+	System.out.printf("1 2 3 4");
+
+	}
+
+}
